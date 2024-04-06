e5cf723258aecceda4c5bcc7870b7745ec3bfc87
TanDuc <140944419+Ducinviciable@users.noreply.github.com>
2024-04-06 20:55:53 +0700

    Update new TextView (edit <redo undo save as>)

--- NOTEPAD/TextView.java
+++ NOTEPAD/TextView.java
@@ -5,12 +5,14 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import javax.swing.undo.UndoManager;
 // import Model.TextModel;
 
 public class TextView extends JFrame {
     private JTextArea textArea;
     private JFileChooser fileChooser;
     private TextModel model;
+    private UndoManager undoManager;
 
     public TextView(TextModel model) {
         this.model = model;
@@ -18,13 +20,26 @@ public class TextView extends JFrame {
         fileChooser = new JFileChooser();
         JScrollPane scrollPane = new JScrollPane(textArea);
         add(scrollPane);
+        undoManager = new UndoManager();
+        textArea.getDocument().addUndoableEditListener(undoManager);
 
         // Menu
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
         JMenuItem openItem = new JMenuItem("Open");
         JMenuItem saveItem = new JMenuItem("Save");
         JMenuItem exitItem = new JMenuItem("Exit");
+        JMenuItem saveAsItem = new JMenuItem("Save as");
+        JMenuItem undoItem = new JMenuItem("Undo");
+        JMenuItem redoItem = new JMenuItem("Redo");
+
+        saveAsItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveAsFile();
+            }
+        });
 
         openItem.addActionListener(new ActionListener() {
             @Override
@@ -40,14 +55,33 @@ public class TextView extends JFrame {
             }
         });
 
+        undoItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                undo();
+            }
+        });
+
+        redoItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                redo();
+            }
+        });
+
         exitItem.addActionListener(e -> System.exit(0));
 
-        fileMenu.add(openItem);
-        fileMenu.add(saveItem);
-        fileMenu.add(exitItem);
         menuBar.add(fileMenu);
-        setJMenuBar(menuBar);
+        fileMenu.add(saveItem);
+        fileMenu.add(saveAsItem);
+        fileMenu.add(openItem);
+        fileMenu.add(exitItem);
 
+        menuBar.add(editMenu);
+        editMenu.add(undoItem);
+        editMenu.add(redoItem);
+
+        setJMenuBar(menuBar);
         setTitle("NotePad");
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -69,4 +103,27 @@ public class TextView extends JFrame {
             model.saveFile(file, textArea.getText());
         }
     }
+
+    // Save as
+    private void saveAsFile() {
+        if (fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            model.saveFile(file, textArea.getText());
+        }
+    }
+
+    // Undo
+    private void undo() {
+        if (undoManager.canUndo()) {
+            undoManager.undo();
+        }
+    }
+
+    // Redo
+    private void redo() {
+        if (undoManager.canRedo()) {
+            undoManager.redo();
+        }
+    }
+
 }
